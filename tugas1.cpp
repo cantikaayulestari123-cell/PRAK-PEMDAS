d43b097dcbbcbf576ac7748715c7047c497c6e8c
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-03-03 21:26:40 +0700

    menambahkan jari jari lingkaran

--- tugas1.cpp
+++ tugas1.cpp
@@ -67,3 +67,8 @@ using namespace std;
 //deklarasi variable
 float jarijari;
 float phi = 3.14159;
+//bagian fungsi
+void awal(){
+    cout << "masukkan jarijari lingkaran: ";
+    cin >> jarijari;
+}
